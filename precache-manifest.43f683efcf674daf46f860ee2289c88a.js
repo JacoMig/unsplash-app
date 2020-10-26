d737aec3ad1973092b953c9ae897908fed36a502
Jacopo <jacomig@gmail.com>
2020-10-26 11:11:09 +0100

    Updates

---
+++ precache-manifest.43f683efcf674daf46f860ee2289c88a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b97f0b633f0496157a9d834ac1e8bb9",
+    "url": "/unsplash-app/index.html"
+  },
+  {
+    "revision": "a6d97148ef10b996b1e9",
+    "url": "/unsplash-app/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "2486868f08939b501cf8",
+    "url": "/unsplash-app/static/css/main.ab2b1472.chunk.css"
+  },
+  {
+    "revision": "a6d97148ef10b996b1e9",
+    "url": "/unsplash-app/static/js/2.d9962b7e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/unsplash-app/static/js/2.d9962b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2486868f08939b501cf8",
+    "url": "/unsplash-app/static/js/main.6d08e87c.chunk.js"
+  },
+  {
+    "revision": "f5a631b79eceb9ea14fa",
+    "url": "/unsplash-app/static/js/runtime-main.98e5fda9.js"
+  },
+  {
+    "revision": "ba07b64dc9724ee59f3166cc1b77b6e9",
+    "url": "/unsplash-app/static/media/unsplash_logo.ba07b64d.svg"
+  }
+]);
